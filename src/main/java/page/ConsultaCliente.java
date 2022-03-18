6ad4a7fb3e8db6db1e2bf40dc922097d279535be
Roger <roger.gonzalez.ext@colcomercio.com.co>
2022-03-18 12:03:12 -0500

    commit 18032022

---
+++ src/main/java/page/ConsultaCliente.java
@@ -0,0 +1,31 @@
+package page;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
+
+@DefaultUrl("http://10.181.3.183:8085/cmpqr_cartera/")
+
+public class ConsultaCliente extends PageObject {
+
+    By textusuario = By.name("usuario");
+    By textpassword = By.name("password");
+    By btningresar = By.id("ingresa");
+
+
+    public void ingresarUsuario(String usuario) {
+        getDriver().findElement(textusuario).sendKeys(usuario);
+    }
+
+    public void ingresarClave(String clave) {
+        getDriver().findElement(textpassword).sendKeys(clave);
+    }
+
+    public void clicIngresar() {
+        getDriver().findElement(btningresar).click();
+    }
+
+    public void salirChrome() {
+        getDriver().close();
+    }
+}
