16ea31b7a9c0b31f9997f1132aa6c38eb76e0922
Roger <roger.gonzalez.ext@colcomercio.com.co>
2022-04-04 08:49:12 -0500

    04 abril 20222

---
+++ src/main/java/runners/steps/ConsultaClienteSteps.java
@@ -0,0 +1,38 @@
+package runners.steps;
+
+
+import runners.page.ConsultaCliente;
+import net.thucydides.core.annotations.Step;
+
+
+public class ConsultaClienteSteps {
+
+
+    ConsultaCliente consultaPOM = new ConsultaCliente();
+
+    @Step
+    public void abrirCRMCartera() {
+        consultaPOM.open();
+    }
+
+    @Step
+    public void escribirUsuario(String usuario) {
+        consultaPOM.ingresarUsuario(usuario);
+
+    }
+
+    @Step
+    public void escribirClave(String clave) {
+        consultaPOM.ingresarClave(clave);
+    }
+
+    @Step
+    public void clicIngresar() {
+        consultaPOM.clicIngresar();
+    }
+
+    @Step
+    public void cerrarChrome() {
+        consultaPOM.salirChrome();
+    }
+}
